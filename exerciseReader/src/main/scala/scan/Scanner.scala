ca89a8d711baeec9c3a952a729af6f0877d1557f
Terry Dai <terry.dai@rea-group.com>
2018-08-15 16:24:08 +1000

    Exercise Reader
    Refactor `takeTopN`, `FileSize.ofFile` and cleanup.

--- exerciseReader/src/main/scala/scan/Scanner.scala
+++ exerciseReader/src/main/scala/scan/Scanner.scala
@@ -45,23 +45,22 @@ object Scanner {
   def pathScan[R: _task: _filesystem: _config](path: FilePath): Eff[R, PathScan] = path match {
     case f: File =>
       for {
-        fs <- ask[R, Filesystem]
-        filesize = FileSize.ofFile(f, fs)
+        filesize <- FileSize.ofFile(f)
       } yield PathScan(SortedSet(filesize), filesize.size, 1)
     case dir: Directory =>
       for {
-        config <- ask[R, ScanConfig]
-        topN = takeTopN(config)
+        topNMonoid <- takeTopN
         filesystem <- ask[R, Filesystem]
         childScans <- filesystem.listFiles(dir).traverse(pathScan[R](_))
-      } yield childScans.combineAll(topN)
+      } yield childScans.combineAll(topNMonoid)
     case Other(_) =>
       PathScan.empty.pureEff[R]
   }
 
-
-  def takeTopN(config: ScanConfig): Monoid[PathScan] =
-    new Monoid[PathScan] {
+  def takeTopN[R: _config](): Eff[R, Monoid[PathScan]] =
+    for{
+      config <- ask[R, ScanConfig]
+    } yield new Monoid[PathScan] {
       def empty: PathScan = PathScan.empty
 
       def combine(p1: PathScan, p2: PathScan): PathScan = PathScan(
@@ -120,7 +119,10 @@ case class FileSize(file: File, size: Long)
 
 object FileSize {
 
-  def ofFile(file: File, fs: Filesystem) = FileSize(file, fs.length(file))
+  def ofFile[R: _filesystem](file: File): Eff[R, FileSize] =
+    for{
+      fs <- ask[R, Filesystem]
+    } yield FileSize(file, fs.length(file))
 
   implicit val ordering: Ordering[FileSize] = Ordering.by[FileSize, Long](_.size).reverse
 
